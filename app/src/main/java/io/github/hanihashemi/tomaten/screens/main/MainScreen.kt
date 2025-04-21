2e97922d0be9c332f28439a43c9fdb67e2a3ed84
Hani <jhanihashemi@gmail.com>
2025-04-21 16:46:07 +0200

    Implement basic layout of the Main Screen

--- app/src/main/java/io/github/hanihashemi/tomaten/screens/main/MainScreen.kt
+++ app/src/main/java/io/github/hanihashemi/tomaten/screens/main/MainScreen.kt
@@ -1,22 +1,40 @@
 package io.github.hanihashemi.tomaten.screens.main
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import io.github.hanihashemi.tomaten.Button
+import io.github.hanihashemi.tomaten.ButtonStyles
+import io.github.hanihashemi.tomaten.screens.main.components.TopBar
+import io.github.hanihashemi.tomaten.theme.Dimens
 import io.github.hanihashemi.tomaten.theme.TomatenTheme
 
 @Composable
 fun MainScreen() {
-    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-        Column(modifier = Modifier.padding(innerPadding)) {
-            Text(
-                text = "Hello Hani!",
-            )
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = { TopBar() },
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+                .padding(Dimens.PaddingNormal),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceEvenly,
+        ) {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Button("Start", modifier = Modifier.widthIn(180.dp)) { }
+                Button("Focus", style = ButtonStyles.Secondary) { }
+            }
         }
     }
 }
