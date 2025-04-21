cee1f5012583dc18e182eaa126f80fa3b512c5d8
Hani <jhanihashemi@gmail.com>
2025-04-21 16:19:10 +0200

    feat: Add vibrate utility function

---
+++ app/src/main/java/io/github/hanihashemi/tomaten/util/VibrateUtil.kt
@@ -0,0 +1,24 @@
+package io.github.hanihashemi.tomaten.util
+
+import android.content.Context
+import android.os.Build
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.os.VibratorManager
+
+object VibrateUtil {
+    fun vibrate(context: Context) {
+        val vibrator = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            val manager =
+                context.getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
+            manager.defaultVibrator
+        } else {
+            @Suppress("DEPRECATION")
+            context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+        }
+
+        val vibrationEffect = VibrationEffect.createOneShot(40, 100)
+
+        vibrator.vibrate(vibrationEffect)
+    }
+}
