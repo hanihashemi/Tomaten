07cb8cf444ac1d4d9bfbbb0ccbfb0db7b143e863
Hani <jhanihashemi@gmail.com>
2025-04-21 16:19:27 +0200

    Set the minimum SDK version to 26

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "io.github.hanihashemi.tomaten"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
