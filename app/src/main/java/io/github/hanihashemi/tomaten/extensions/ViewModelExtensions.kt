f074f9de14e384fa59a24dd2b2afc44060476ead
Hani <jhanihashemi@gmail.com>
2025-07-09 18:25:06 +0200

    Add launchSafely ViewModel extension

---
+++ app/src/main/java/io/github/hanihashemi/tomaten/extensions/ViewModelExtensions.kt
@@ -0,0 +1,15 @@
+package io.github.hanihashemi.tomaten.extensions
+
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import timber.log.Timber
+import kotlin.coroutines.CoroutineContext
+
+private val coroutineExceptionHandler = CoroutineExceptionHandler { _, t -> Timber.e(t) }
+
+internal fun CoroutineScope.launchSafely(
+    context: CoroutineContext,
+    launchBody: suspend CoroutineScope.() -> Unit
+): Job = launch(context + coroutineExceptionHandler) { launchBody.invoke(this) }
