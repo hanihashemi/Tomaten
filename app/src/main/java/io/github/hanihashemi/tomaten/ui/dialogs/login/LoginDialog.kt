af21fc8efc512f13311a71572f0f32ceb8f92843
Hani <jhanihashemi@gmail.com>
2025-07-09 18:25:34 +0200

    Refactor: Improve LoginDialog UI and functionality

--- app/src/main/java/io/github/hanihashemi/tomaten/ui/dialogs/login/LoginDialog.kt
+++ app/src/main/java/io/github/hanihashemi/tomaten/ui/dialogs/login/LoginDialog.kt
@@ -1,39 +1,160 @@
 package io.github.hanihashemi.tomaten.ui.dialogs.login
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import io.github.hanihashemi.tomaten.Actions
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import io.github.hanihashemi.tomaten.Button
-import io.github.hanihashemi.tomaten.UIState
+import io.github.hanihashemi.tomaten.ButtonStyles
+import io.github.hanihashemi.tomaten.R
+import io.github.hanihashemi.tomaten.theme.Dimens
+import io.github.hanihashemi.tomaten.theme.TomatenTheme
+import io.github.hanihashemi.tomaten.ui.actions.Actions
+import io.github.hanihashemi.tomaten.ui.actions.previewActions
+import io.github.hanihashemi.tomaten.ui.states.LoginUiState
+import io.github.hanihashemi.tomaten.ui.states.UIState
 
 @Composable
 fun LoginDialog(uiState: UIState, actions: Actions) {
-    if (uiState.loggingDialogVisible) {
-        AlertDialog(
-            onDismissRequest = { actions.login.dismissDialog() },
-            title = {
-                Text(text = "Login")
-            },
-            text = {
-                Text("Here you can show login instructions or progress.")
-            },
-            confirmButton = {
-                Button(
-                    text = "Proceed",
-                    onClick = {
+    if (!uiState.login.isDialogVisible) return
 
+    AlertDialog(
+        onDismissRequest = { actions.login.displayDialog(false) },
+        title = {
+            Text(text = "Sign in to continue")
+        },
+        text = {
+            Column(
+                verticalArrangement = Arrangement.spacedBy(Dimens.PaddingXXXSmall),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text("Please sign in with your Google account to sync your Pomodoro data.")
+                Spacer(modifier = Modifier.height(Dimens.PaddingXLarge))
+
+
+                when {
+                    uiState.login.isLoading -> {
+                        CircularProgressIndicator()
                     }
-                )
-            },
-            dismissButton = {
-                Button(
-                    text = "Cancel",
-                    onClick = {
-                        actions.login.dismissDialog()
+
+                    else -> {
+                        LoginButtons(actions, uiState)
                     }
-                )
+                }
+
             }
+        },
+        confirmButton = {},
+    )
+}
+
+@Composable
+private fun LoginButtons(actions: Actions, uiState: UIState) {
+    ErrorMessage(uiState)
+    Button(
+        modifier = Modifier
+            .fillMaxWidth(),
+        onClick = {
+            actions.login.login()
+        },
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.android_neutral_rd_na),
+                contentDescription = "Google logo",
+                modifier = Modifier.size(24.dp)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text("Sign in with Google")
+        }
+    }
+    Button(
+        text = "Dismiss",
+        style = ButtonStyles.Secondary,
+    ) { actions.login.displayDialog(false) }
+}
+
+@Composable
+private fun ErrorMessage(uiState: UIState) {
+    if (uiState.login.errorMessage == null) return
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Icon(
+            imageVector = Icons.Default.Close,
+            contentDescription = "Error",
+            tint = Color.Red,
+            modifier = Modifier.size(Dimens.PaddingNormal)
+        )
+        Spacer(modifier = Modifier.width(Dimens.PaddingXXSmall))
+        Text(
+            text = uiState.login.errorMessage,
+            color = Color.Red,
+        )
+    }
+
+    Spacer(modifier = Modifier.height(Dimens.PaddingXSmall))
+}
+
+@Preview
+@Composable
+private fun LoginDialogPreview() {
+    TomatenTheme {
+        LoginDialog(
+            uiState = UIState(
+                login = LoginUiState(isDialogVisible = true)
+            ),
+            actions = previewActions,
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun LoginDialogLoadingPreview() {
+    TomatenTheme {
+        LoginDialog(
+            uiState = UIState(
+                login = LoginUiState(isDialogVisible = true, isLoading = true)
+            ),
+            actions = previewActions,
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun LoginDialogErrorPreview() {
+    TomatenTheme {
+        LoginDialog(
+            uiState = UIState(
+                login = LoginUiState(isDialogVisible = true, errorMessage = "Something went wrong.")
+            ),
+            actions = previewActions,
         )
     }
 }
